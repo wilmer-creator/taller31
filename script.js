a9046abc66bd0beadaba217c98c952298e4cb142
wilmer-creator <est.wilmer.salamanca@unimilitar.edu.co>
2026-05-08 12:09:53 -0500

    planteamiento inicial del codigo

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Obtener el elemento canvas desde el HTML
+const canvas = document.getElementById("canvas");
+
+// Obtener el contexto 2D
+const ctx = canvas.getContext("2d");
+
+
+// Verificar que JavaScript funciona
+console.log("Canvas iniciado correctamente");
